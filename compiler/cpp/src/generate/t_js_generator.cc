d61c96455cfe5be46dd8135e159709566531fc14
Nhan Phan <nhan.phan@gmail.com>
2013-02-19 13:02:47 -0800

    generate reverse maps

--- compiler/cpp/src/generate/t_js_generator.cc
+++ compiler/cpp/src/generate/t_js_generator.cc
@@ -350,7 +350,8 @@ void t_js_generator::generate_enum(t_enum* tenum) {
   vector<t_enum_value*>::iterator c_iter;
   for (c_iter = constants.begin(); c_iter != constants.end(); ++c_iter) {
     int value = (*c_iter)->get_value();
-    f_types_ << "'" << (*c_iter)->get_name() << "' : " << value;
+    f_types_ << "'" << (*c_iter)->get_name() << "' : " << value << "," << endl;
+    f_types_ << "'" << value << "' : '" << (*c_iter)->get_name() << "'";
     if (c_iter != constants.end()-1) {
         f_types_ << ",";
     }
